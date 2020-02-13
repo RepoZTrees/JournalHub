e5eb0edfa2bacc303824ff45c24a1f822fc2e54b
zuhara <zuhara@hamon.in>
2020-02-13 16:01:52 +0530

    Changed sys library to argparse because it allows users to see the arguments from command line using help option

--- journal/commands.py
+++ journal/commands.py
@@ -4,8 +4,18 @@ import shutil
 import os
 import os.path
 from . import parser
+import argparse
+
+
+def arg_parse():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('function',help='$ journal init  -->  Initialzes blog templates. $ journal generate  -->  Generates html files', choices = ["init","generate"])
+    args = arg_parser.parse_args()
+    return args.function
 
 def main():
+    argument = arg_parse()
+
     this_dir,this_filename = os.path.split(__file__)
     source_path = os.path.join(this_dir,'assets','config.ini')
     destination_path = os.path.join(os.getcwd(),'config.ini')
@@ -13,12 +23,11 @@ def main():
     source_path1 = os.path.join(this_dir,'assets','templates')
     destination_path1 = os.path.join(os.getcwd(),'templates')
 
-    if sys.argv[1] == 'init':
+    if argument == 'init':
         dest = shutil.copyfile(source_path, destination_path)
         d = shutil.copytree(source_path1, destination_path1)
-        print("Folder initialized.")
-    elif sys.argv[1] == 'generate':
+        print("Blog templates created")
+    elif argument == 'generate':
         path = os.getcwd()
         parser.md_to_html(path)
-    else:
-        print('wrong command')
+    
